07e858e7a59484fae63e8cbe5de3688962476b99
songyi225 <sy920225@naver.com>
2024-08-11 14:41:46 +0900

    [구현] Square 컴포넌트 속성검사 추가

--- homework-04/src/tic-tac-toe/components/Square/Square.jsx
+++ homework-04/src/tic-tac-toe/components/Square/Square.jsx
@@ -1,14 +1,23 @@
-import { node } from 'prop-types';
+import { func, node } from 'prop-types';
 import S from './Square.module.css';
 
 Square.propTypes = {
   children: node,
+  onPlay: func,
 };
 
-function Square({ children }) {
+// 상태를 가지지 않는(Stateless) 컴포넌트
+function Square({ children, onPlay, ...restProps }) {
+  // [파생된 상태]
   const isDisabled = !!children;
+
   return (
-    <button className={S.component} disabled={isDisabled}>
+    <button
+      className={S.component}
+      onClick={onPlay}
+      disabled={isDisabled}
+      {...restProps}
+    >
       {children}
     </button>
   );
