19e8daf79f9e85d5d70413868737ed4dcb073aab
songyi225 <sy920225@naver.com>
2024-08-11 14:55:52 +0900

    [구현] Squares 컴포넌트의 판을 순환하며 Square 컴포넌트를 리스트 렌더링

--- homework-04/src/tic-tac-toe/components/Squares/Squares.jsx
+++ homework-04/src/tic-tac-toe/components/Squares/Squares.jsx
@@ -1,12 +1,38 @@
+import S from './Squares.module.css';
 import Square from '../Square/Square';
-import { PLAYER } from '@/tic-tac-toe/constants';
+import { useState } from 'react';
+import { PLAYER, PLAYER_COUNT, INITIAL_SQUARES } from '@/tic-tac-toe/constants';
 
 function Squares() {
+  const [squares, setSquares] = useState(INITIAL_SQUARES);
+
+  const playGame = (index) => {
+    setSquares((prevSquares) => {
+      const nextSquares = prevSquares.map((square, squareIndex) => {
+        if (squareIndex === index) {
+          return currentPlayer;
+        }
+        return square;
+      });
+      return nextSquares;
+    });
+  };
+
+  const gameIndex = squares.filter(Boolean).length;
+
+  const isPlayerOneTurn = gameIndex % PLAYER_COUNT === 0;
+
+  const currentPlayer = isPlayerOneTurn ? PLAYER.ONE : PLAYER.TWO;
+
   return (
-    <div className="Squares">
-      <Square></Square>
-      <Square>{PLAYER.ONE}</Square>
-      <Square>{PLAYER.TWO}</Square>
+    <div className={S.component}>
+      {squares.map((square, index) => {
+        return (
+          <Square key={index} onPlay={() => playGame(index)}>
+            {square}
+          </Square>
+        );
+      })}
     </div>
   );
 }
